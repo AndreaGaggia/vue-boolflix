c6a9de93adfe4892c23260da057152be8f999d14
AndreaG <gaggiandrea@gmail.com>
2021-01-12 14:56:19 +0100

    milestone 2 done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,11 @@
 //* API Key = 211f9317ff0f147fee603f1b9da7607e
 
+/*
+esempio chiamata per serie tv
+https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=s
+crubs
+*/
+
 const app = new Vue({
     el: "#app",
     data: {
@@ -8,14 +14,31 @@ const app = new Vue({
     },
     methods: {
         search() {
-            axios
-                .get(
-                    `https://api.themoviedb.org/3/search/movie?api_key=211f9317ff0f147fee603f1b9da7607e&query=${this.searchString}`
-                )
-                .then((response) => {
-                    let results = response.data.results;
-                    this.films = results;
+            this.getFilms();
+            this.getSeries();
+
+            Promise.all([this.getFilms(), this.getSeries()]).then((results) => {
+                let films = results[0].data.results;
+                let series = results[1].data.results;
+                this.films = films.concat(series);
+
+                this.films.map((film) => {
+                    return (film.stars = Math.round(film.vote_average / 2));
                 });
+                this.films.map((film) => {
+                    return (film.flag = `https://www.countryflags.io/${film.original_language}/flat/32.png`);
+                });
+            });
+        },
+        getFilms() {
+            return axios.get(
+                `https://api.themoviedb.org/3/search/movie?api_key=211f9317ff0f147fee603f1b9da7607e&query=${this.searchString}`
+            );
+        },
+        getSeries() {
+            return axios.get(
+                `https://api.themoviedb.org/3/search/tv?api_key=211f9317ff0f147fee603f1b9da7607e&language=it_IT&query=${this.searchString}`
+            );
         },
     },
 });
